74380531adf3992acc6bff695f2e63fad07053e2
krzysztof sabatowski <krzysztof.sabatowski@gmail.com>
2018-12-16 23:25:36 +0100

    nowy projekt konwerter jednostek - runner modified3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,52 +2,127 @@ import java.util.Scanner;
 
 public class Runner {
 
+
+    private static final double MILES_TO_KM_FACTOR = 1.609;
+    private static final double KILOGRAM_TO_POUND_FACTOR = 0.45;
+
     /**
-     * konwerter jednostek Lb to Kg
+     * konwerter jednostek Funty na Kilogramy
      * */
 
     static double lbToKg(int lb){
-        final double KG = 0.45;
-        return lb*KG;
+        return lb*KILOGRAM_TO_POUND_FACTOR;
     }
 
+    /**
+    * konwerter jednostek mile na kilometry
+    * */
 
-    private static final double MILES_TO_KM_FACTOR = 1.609;
+    public static double mi2km (double miles) {
+        return miles*MILES_TO_KM_FACTOR;
+    }
+
+    /**
+     * konwerter costam
+     * */
+
+    public static void costam(double tamcos){
+
+    }
+
+    /**
+     * konwerter stopni Celcjusza na Fahrenheita
+     * */
+
+    static void celcjuszToFahrenheit(double celcjusz){
+
+    }
+
+    /**
+     * pokaz menu na zadanie
+     * */
+
+    static void showMenu(){
+        System.out.println("0. wyjscie z programu");
+        System.out.println("1. konwerter stopni Celcjusza na Fahrenheita");
+        System.out.println("2. konwerter Funtow na Kilogramy");
+        System.out.println("3. konwerter Mile na Kilometry");
+        System.out.println("4. konwerter czegos tam tam tam");
+        System.out.println("5. pokaz menu");
+        System.out.println();
+        System.out.print("Podaj numer menu od 0 do 5 : ");
+        choseMenu(getNumber());
+    }
+
+    /**
+     * pobierz liczbe int z klawiatury
+     * */
+
+    static int getNumber(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt();
+    }
+
+    /**
+     * wybierz jedna z opcji dostepna w menu
+     * */
+
+    static void choseMenu(int menuNumber){
+        switch (menuNumber){
+            case 0:
+                System.out.println("wyszedles z programu");
+                System.exit(0);
+                break;
+
+            case 1:
+                System.out.print("wybrales konwerter stopni Celcjusza na Fahrenheita, podaj liczbe stopni Celcjusza: ");
+                int celcjusz = getNumber();
+                //System.out.printf("%d Celcjusza to %f Fahrenheita \n", celcjusz, celcjuszToFahrenheit(celcjusz));
+                System.out.print("Co dalej, wybierz 0 - 5: ");
+                System.out.print("Co dalej, wybierz 0 - 5: ");
+                choseMenu(getNumber());
+                break;
+
+            case 2:
+                System.out.print("wybrales konwerter Funtow na Kilogramy, podaj liczbe Funtow: ");
+                int lb = getNumber();
+                System.out.printf("%d Funtow to %f Kilogramow \n", lb, lbToKg(lb));
+                System.out.print("Co dalej, wybierz 0 - 5: ");
+                choseMenu(getNumber());
+                break;
+
+            case 3:
+                System.out.print("wybrales konwerter Mile na Kilometry, podaj liczbe Mil: ");
+                int mile = getNumber();
+                System.out.printf("%d Mil to %f km", mile, mi2km(mile));
+                System.out.print("Co dalej, wybierz 0 - 5: ");
+                choseMenu(getNumber());
+                break;
+
+            case 4:
+                System.out.print("wybrales konwerter czegos tam tam tam, podaj liczbe czegos tam: ");
+                int costam = getNumber();
+                //System.out.printf("%d Costam to %f Tamcos", costam, costam(costam));
+                System.out.print("Co dalej, wybierz 0 - 5: ");
+                choseMenu(getNumber());
+                break;
+
+            case 5:
+                showMenu();
+                break;
+
+            default: {
+                if (menuNumber < 0 || menuNumber > 5)
+                    System.out.println("Bledny numer menu, wybierz ponownie 0 - 5: ");
+                choseMenu(getNumber());
+            }
+        }
+    }
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("podaj numer menu od 1 do 4 ");
-        int menu = scanner.nextInt();
-        switch (menu){
-            case 1:
-                System.out.println("wybrales 1");
+        showMenu();
+        choseMenu(getNumber());
 
-                break;
-            case 2:
-                System.out.println("wybrales 2");
-                Scanner lbScanner = new Scanner(System.in);
-                int lb = lbScanner.nextInt();
-                System.out.println(Runner.lbToKg(lb));
-                break;
-            case 3:
-                System.out.println("wybrales 3");
-                Scanner mileScanner = new Scanner(System.in);
-                double mile = mileScanner.nextDouble();
-//                System.out.println(Runner.mi2km(double mile));
-                break;
-            case 4:
-                System.out.println("wybrales 4");
-                break;
-                default: {
-                    if (menu < 1 || menu > 4)
-                    System.out.println("wybierz ponownie");
-                }
-        }
-
-
-    }
-    public static double mi2km (double miles) {
-        return miles*MILES_TO_KM_FACTOR;
     }
 }
