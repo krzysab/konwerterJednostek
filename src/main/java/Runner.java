40124601ab1f6c66e0e88ecee101d2be7d381007
jan kubal <jankubal@gmail.com>
2018-12-16 15:37:26 +0100

    miles to kilometers - constant factor

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,8 +1,11 @@
 public class Runner {
+
+    private static final double MILES_TO_KM_FACTOR = 1.609;
+
     public static void main(String[] args) {
 
     }
     public static double mi2km (double miles) {
-        return miles*1.609;
+        return miles*MILES_TO_KM_FACTOR;
     }
 }
