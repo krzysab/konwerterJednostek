db470f501b074c094388eacaf45dfd8882d20e79
Lukasz Jonczy <jonczy.lukasz@gmail.com>
2018-12-16 15:45:57 +0100

    Zamienia farenchajta na celsjusza

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,15 @@
 public class Runner {
     public static void main(String[] args) {
+        float temperature;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Wpisz temp. w Fahrenheit");
+        temperature = in.nextInt();
+
+        temperature = ((temperature - 32)*5)/9;
+
+        System.out.println("temperata w Celsjuszach = " + temperature);
+    }
 
     }
 }
